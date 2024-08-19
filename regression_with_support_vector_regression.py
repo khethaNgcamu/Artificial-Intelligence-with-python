4b0ab61a355fc61eb0346369e1b72298096a1fa5
Smanga Ngcamu <ngcamukhetha@gmail.com>
2024-08-19 18:12:02 +0200

    uncomented linear mkdel import

--- regression_with_support_vector_regression.py
+++ regression_with_support_vector_regression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.svm import SVR
 
@@ -27,8 +27,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-
-
 
 # Initialize the model
 svr = SVR(kernel='rbf')
