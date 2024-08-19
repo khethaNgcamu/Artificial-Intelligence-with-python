6c427c63597a54b6ad8891de27f80cc817b2dfbe
Smanga Ngcamu <ngcamukhetha@gmail.com>
2024-08-19 18:00:07 +0200

    model cross validated

--- regression_with_linear_regression.py
+++ regression_with_linear_regression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -30,6 +30,9 @@ X_test = scaler.transform(X_test)
 #Initialise the model
 lin_reg = LinearRegression()
 
+# Perform cross-validation
+cv_scores = cross_val_score(lin_reg, X_train, y_train, cv=5)
+
 #training the model
 lin_reg.fit(X_train, y_train)
 
@@ -40,3 +43,7 @@ y_pred = lin_reg.predict(X_test)
 print("================================================================")
 print('Mean square error: ', mean_squared_error(y_test, y_pred))
 print('R2 score: ', r2_score(y_test, y_pred))
+
+# Cross-Validation Results
+print(f"Cross-Validation Accuracy: {cv_scores.mean()}")
+print(f"Cross-Validation Standard Deviation: {cv_scores.std()}")
--- regression_with_support_vector_regression.py
+++ regression_with_support_vector_regression.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.svm import SVR
 
 #load the data
 file_path = 'C:/Users/Khetha/Desktop/Neural-Network-Exercises/housing_dataset.csv'
@@ -27,10 +28,13 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-from sklearn.svm import SVR
+
 
 # Initialize the model
 svr = SVR(kernel='rbf')
+
+# Perform cross-validation
+cv_scores = cross_val_score(svr, X_train, y_train, cv=5, scoring='r2')
 
 # Train the model
 svr.fit(X_train, y_train)
@@ -42,3 +46,7 @@ y_pred = svr.predict(X_test)
 print("============================================================")
 print('Mean Squared Error:', mean_squared_error(y_test, y_pred))
 print('R-squared:', r2_score(y_test, y_pred))
+
+# Cross-Validation Results
+print(f"Cross-Validation Accuracy: {cv_scores.mean()}")
+print(f"Cross-Validation Standard Deviation: {cv_scores.std()}")
